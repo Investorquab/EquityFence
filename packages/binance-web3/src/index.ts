45fb0e2c1b90c65282dc0f4381882f62c1dda566
quabnation <quadribello3@gmail.com>
2026-09-25 03:30:14 +0100

    feat: add Binance Web3 RWA data client

---
+++ packages/binance-web3/src/index.ts
@@ -0,0 +1,9 @@
+export { BinanceWeb3Client } from "./client.js";
+export type { BinanceWeb3Config } from "./client.js";
+export type {
+  BinanceResponse,
+  RwaSearchAsset,
+  RwaSearchResult,
+  RwaToken,
+  RwaTokenPrice,
+} from "./types.js";
