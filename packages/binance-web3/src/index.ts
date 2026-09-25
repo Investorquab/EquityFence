6c450d5db7b47fe5edecb90a041d6ae955dc47fc
quabnation <quadribello3@gmail.com>
2026-09-25 03:44:55 +0100

    feat(transaction): export simulation adapter

--- packages/binance-web3/src/index.ts
+++ packages/binance-web3/src/index.ts
@@ -1,5 +1,16 @@
 export { BinanceWeb3Client } from "./client.js";
+export { BinanceTransactionClient } from "./transaction.js";
+
 export type { BinanceWeb3Config } from "./client.js";
+
+export type {
+  EvmTransaction,
+  SimulationRequest,
+  SimulationResult,
+  BalanceChange,
+  AllowanceChange,
+} from "./transaction.js";
+
 export type {
   BinanceResponse,
   RwaSearchAsset,
