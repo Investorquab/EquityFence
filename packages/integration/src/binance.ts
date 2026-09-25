6953d65c504b1cb0634e31abc6c4402737bf2128
quabnation <quadribello3@gmail.com>
2026-09-25 00:01:33 -0300

    feat(integration): add Binance RWA state adapter

---
+++ packages/integration/src/binance.ts
@@ -0,0 +1,18 @@
+import {
+  BinanceWeb3Client,
+  type RwaToken,
+} from "@equityfence/binance-web3";
+import type { RwaStateSource } from "./model.js";
+
+export class BinanceRwaStateSource implements RwaStateSource {
+  constructor(
+    private readonly client: BinanceWeb3Client,
+    private readonly platformId?: "ondo" | "bstock",
+  ) {}
+
+  async getAsset(assetId: string): Promise<RwaToken> {
+    return (
+      await this.client.getRwaTokenByContract(assetId, this.platformId)
+    ).data;
+  }
+}
