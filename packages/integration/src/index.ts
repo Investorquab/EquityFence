cb736b929fa879930a2ef1e838dd2460799c95ab
quabnation <quadribello3@gmail.com>
2026-09-25 03:45:11 +0100

    feat(integration): export guarded simulation

--- packages/integration/src/index.ts
+++ packages/integration/src/index.ts
@@ -1,7 +1,13 @@
-export { evaluateSafety, snapshotFromRwaToken } from "./model.js";
+export {
+  evaluateSafety,
+  snapshotFromRwaToken,
+  simulateIfAllowed,
+} from "./model.js";
 
 export type {
+  GuardedSimulationResult,
   RwaStateSource,
   SafetyCheckInput,
   SafetyCheckResult,
+  SimulationSource,
 } from "./model.js";
